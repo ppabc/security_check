20732f24d814228bfce4f37e779a56a6a7abb670
robert.yu <ppabc@qq.com>
2014-10-31 15:24:58 +0800

    bat

---
+++ win/firewall.bat
@@ -0,0 +1,17 @@
+echo off 
+cls 
+set var=30000 
+set end=30010 
+:continue
+set /a var+=1 
+echo add port %var% 
+netsh firewall add portopening TCP %var% ftp_data_%var% 
+if %var% lss %end% goto continue 
+
+netsh firewall set portopening all 80 aqzt.com-80 enable 
+netsh firewall set portopening all 443 aqzt.com-443 enable 
+netsh firewall set portopening all 3399 aqzt.com-3399 enable 
+netsh firewall set portopening all 3299 aqzt.com-3299 enable 
+
+echo complete 
+pause
